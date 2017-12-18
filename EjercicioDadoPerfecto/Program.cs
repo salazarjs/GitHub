c78f8a991ef9b67c3879a84f373fb94bbb6f6f88
salazarjs <34646311+salazarjs@users.noreply.github.com>
2017-12-18 02:55:50 -0700

    EjercicioF1NP
    Ejercicio Fase 1 Numero Perfecto

---
+++ EjercicioDadoPerfecto/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace EjercicioDadoPerfecto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("FASE 1: Ejercicio 1 - Numero perfecto \n");
+
+            var numerodado = 0;
+          
+            //captura de numero a validar
+            Console.Write("Escribe un numero: ");
+            
+            numerodado = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("\n");
+
+            //Revisar si es perfecto o no el numero dado 
+            if (NumeroPerfecto(numerodado))
+            {
+                Console.WriteLine("Numero " + numerodado + " es numero PERFECTO");
+            }
+            else
+            {
+                Console.WriteLine("Numero " + numerodado + " NO es numero PERFECTO");
+            }
+            
+            Console.WriteLine("\n");
+            Console.Write("Finalizar... presiona ENTER");
+            Console.ReadLine();
+        }
+
+        public static Boolean NumeroPerfecto(int numeroDado)
+        {
+            var suma = 0;
+
+            for (int i = 1; i < numeroDado; i++)
+            {
+                if ((numeroDado % i) == 0)
+                {
+                    suma = suma + i;
+                }
+            }
+
+            if (numeroDado == suma)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
